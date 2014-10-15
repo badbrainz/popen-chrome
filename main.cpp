f736803ff21db79024ddf5c8ad6a451abbb33b01
wormboy <wormboy@localhost>
2014-10-14 20:15:50 -0700

    switch to standard functions

--- main.cpp
+++ main.cpp
@@ -1,44 +1,41 @@
-#include <stdint.h>
 #include "picojson.h"
+#include "pstream.h"
+#include <stdint.h>
 
-using namespace std;
-
-void send_message(const string& str) {
+void send_message(const std::string& str) {
     uint32_t msglen = str.length();
-    cout.write(reinterpret_cast<char*>(&msglen), 4);
-    cout << str;
+    std::cout.write(reinterpret_cast<char*>(&msglen), 4);
+    std::cout << str << std::flush;
 }
 
 int main(int argc, char* argv[]) {
     uint32_t msglen = 0;
-    cin.read(reinterpret_cast<char*>(&msglen), 4);
+    std::cin.read(reinterpret_cast<char*>(&msglen), 4);
 
-    string message;
+    std::string message;
     for (uint32_t i = 0; i < msglen; i++)
         message += getchar();
 
     picojson::value json;
     const char *msg = message.c_str();
-    string err = picojson::parse(json, msg, msg + strlen(msg));
+    std::string err = picojson::parse(json, msg, msg + strlen(msg));
     if (!err.empty())
         return 1;
 
     picojson::value command = json.get("cmd");
-    if (!command.is<string>())
+    if (!command.is<std::string>())
         return 1;
 
-    FILE* stream = popen(command.to_str().c_str(), "r");
-    if (stream == NULL)
+    redi::ipstream child(command.to_str());
+    if (!child.is_open())
         return 1;
 
-    picojson::object result;
-    char buffer[1024];
-    while (fgets(buffer, 1024, stream) != NULL) {
-        result["message"] = picojson::value(buffer);
-        send_message(picojson::value(result).serialize());
+    picojson::object response;
+    std::string line;
+    while (std::getline(child, line)) {
+        response["message"] = picojson::value(line);
+        send_message(picojson::value(response).serialize());
     }
-
-    pclose(stream);
 
     return 0;
 }
